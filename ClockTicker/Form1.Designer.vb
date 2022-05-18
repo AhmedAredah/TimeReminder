7a2cfddae0ef0326579469f78090f3d68d17e687
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2022-05-18 08:59:41 -0400

    init

---
+++ ClockTicker/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ShowToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ContextMenuStrip1.SuspendLayout
+        Me.SuspendLayout
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = true
+        Me.Timer1.Interval = 500
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"),System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = true
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ShowToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(104, 48)
+        Me.ContextMenuStrip1.Text = "Clock Ticker"
+        '
+        'ShowToolStripMenuItem
+        '
+        Me.ShowToolStripMenuItem.Name = "ShowToolStripMenuItem"
+        Me.ShowToolStripMenuItem.Size = New System.Drawing.Size(103, 22)
+        Me.ShowToolStripMenuItem.Text = "Show"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(103, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = true
+        Me.Label1.Location = New System.Drawing.Point(109, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(271, 97)
+        Me.ControlBox = false
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"),System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Opacity = 0.2R
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Clock Ticker"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.ContextMenuStrip1.ResumeLayout(false)
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents ShowToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ ClockTicker/Form1.vb
@@ -0,0 +1,84 @@
+﻿Public Class Form1
+    Private Spoken As Boolean = True
+    Private Ticker As Boolean = False
+    Private FrmClose As Boolean = False
+'Private Sub LabelControl1_Click( sender As Object,  e As EventArgs) Handles LabelControl1.Click
+'        Timer1.Interval = GetNearstquarter(DateTime.Now) - DateTime.Now.Millisecond
+'        Timer1.Start
+'End Sub
+
+'    ''Private Sub DoStuffThread()
+'    ''    Dim NextExecutionTime As DateTime = DateTime.Now
+'    ''    Dim speaker As New SpeechLib.SpVoice
+
+'    ''    Timer1.Interval = GetNearstquarter(NextExecutionTime) - NextExecutionTime.Millisecond
+
+'    ''    ''nextExecution = New DateTime(nextExecution.Year, nextExecution.Month, nextExecution.Day, 10, 0, 0)
+
+'    ''    'If NextExecutionTime < DateTime.Now Then NextExecutionTime = NextExecutionTime.AddMinutes(15)
+
+'    ''    'Do
+'    ''    '    If NextExecutionTime < DateTime.Now Then
+                
+'    ''    '        speaker.Speak(DateTime.Now.ToString)
+'    ''    '       NextExecutionTime = NextExecutionTime.AddMinutes(15)
+'    ''    '    End If
+
+'    ''    '    Threading.Thread.Sleep(60 * 1000) ' Just sleep 1 minutes
+'    ''    'Loop
+'    ''End Sub
+
+'    Private Function GetNearstquarter(NowDateTime As DateTime) As Integer
+'        While (NowDateTime.Minute mod 15 <> 0)
+'            NowDateTime.AddSeconds(1)
+'        End While
+'        Return NowDateTime.Millisecond
+'    End Function
+
+Private Sub Timer1_Tick( sender As Object,  e As EventArgs) Handles Timer1.Tick
+        If Ticker  Then
+            Label1.Text = format(DateTime.Now, "hh mm tt"): Ticker = False
+        Else
+            Label1.Text = format(DateTime.Now, "hh:mm tt"): Ticker = True
+        End If
+        'LabelControl1.Text = format(DateTime.Now, "hh:mm tt")     
+  
+        If DateTime.Now.Minute mod 15 = 0  and Spoken Then
+            'LabelControl1.Text = DateTime.Now
+            Dim speaker As new SpeechLib.SpVoice
+            speaker.Rate = 1
+            speaker.Speak(format(DateTime.Now, "hh:mm"))
+            Spoken = False
+        ElseIf Spoken = False andalso DateTime.Now.Minute mod 15 <> 0 then
+            Spoken = True
+        End If
+        
+End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        e.Cancel = Not FrmClose
+        Me.Hide
+        NotifyIcon1.Visible = True
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        Label1.Text = format(DateTime.Now, "hh:mm tt")
+
+    End Sub
+
+Private Sub Form1_SizeChanged( sender As Object,  e As EventArgs) Handles MyBase.load
+        Me.Hide
+        NotifyIcon1.Visible = True
+End Sub
+
+Private Sub ShowToolStripMenuItem_Click( sender As Object,  e As EventArgs) Handles ShowToolStripMenuItem.Click, ContextMenuStrip1.MouseDoubleClick
+        Me.Show
+        NotifyIcon1.Visible = False
+End Sub
+
+Private Sub ExitToolStripMenuItem_Click( sender As Object,  e As EventArgs) Handles ExitToolStripMenuItem.Click
+        FrmClose = True
+        Application.Exit
+End Sub
+
+End Class
